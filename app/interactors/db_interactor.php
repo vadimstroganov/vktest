c5713192a810fe87994ecdc52f6067fc62e85b48
Vadim Stroganov <vadimstroganov@gmail.com>
2018-08-31 23:39:24 +0300

    Реализованы функции создания/закрытия соединения с БД

---
+++ app/interactors/db_interactor.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Создание соединения с базой данных
+ *
+ * @return mysqli
+ */
+function create_db_connection() {
+  $host = $_ENV['DB_HOST'];
+  $db   = $_ENV['DB_NAME'];
+  $user = $_ENV['DB_USER'];
+  $pass = $_ENV['DB_PASS'];
+
+  $connection = mysqli_connect($host, $user, $pass, $db);
+
+  // проверка соединения
+  if (mysqli_connect_errno()) {
+    printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+    exit();
+  }
+
+  return $connection;
+}
+
+/**
+ * Закрытие соединения с базой данных
+ *
+ * @param $connection Ссылка на соединение с базой данных
+ *
+ * @return void
+ */
+function close_db_connection($connection) {
+  mysqli_close($connection);
+}
