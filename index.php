9a2b55fb4ff6c075ef1a3b6271e9f60558181e00
Vadim Stroganov <vadimstroganov@gmail.com>
2018-09-01 00:15:48 +0300

    Объявлен путь до моделей. Подключены функции для работы с БД.

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 define('DOCROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
 define('CONTROLLERS_PATH', DOCROOT . 'app/controllers/');
 define('INTERACTORS_PATH', DOCROOT . 'app/interactors/');
+define('MODELS_PATH', DOCROOT . 'app/models/');
 define('VIEWS_PATH', DOCROOT . 'app/views/');
 
 include INTERACTORS_PATH . 'router_interactor.php';
 include INTERACTORS_PATH . 'view_interactor.php';
+include INTERACTORS_PATH . 'db_interactor.php';
 
 const ROUTES = [
   '/' => [ 'controller' => 'items', 'action' => 'index' ]
