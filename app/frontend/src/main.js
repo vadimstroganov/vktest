d5a3545f09a38f648c40b039fd4ea5caf3a5428d
Vadim Stroganov <vadimstroganov@gmail.com>
2018-09-10 23:46:20 +0300

    Из frontend приложения удалены неиспользуемые зависимости

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueSwal from 'vue-swal'
@@ -11,7 +10,6 @@ axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localh
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
 
